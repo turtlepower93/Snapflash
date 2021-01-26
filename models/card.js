a812ad99c57e2d5308bc51dde0e073600f8eb6d2
Richard Williams <richelliswilliams@gmail.com>
2021-01-26 10:31:32 -0800

    change from refencing to embedding

--- models/card.js
+++ models/card.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const cardSchema = new Schema({
-  name: {type: String, required: true},
-  description: {type: String, required:true}
+  word: {type: String, required: true},
+  definition: {type: String, required:true}
 }, {
   timestamps: true,
 });
--- models/deck.js
+++ models/deck.js
@@ -1,9 +1,18 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const cardSchema = new Schema({
+    word: {type: String, required: true},
+    definition: {type: String, required:true}
+  }, {
+    timestamps: true,
+  });
+
 const deckSchema = new Schema({
-  word: {type: String, required: true},
-  definition: {type: String, required:true}
+  name: {type: String, required: true},
+  description: {type: String, required:true},
+  user: {type: Schema.Types.ObjectId, ref: 'User'},
+  cards: [cardSchema],
 }, {
   timestamps: true,
 });
