0131998da679693e94762e722a1e170cd3d59213
Richard Williams <richelliswilliams@gmail.com>
2021-02-02 14:53:18 -0800

    main styling

--- src/components/DecksListContainer/DecksListContainer.jsx
+++ src/components/DecksListContainer/DecksListContainer.jsx
@@ -13,10 +13,14 @@ export default function DecksListContainer({ decks, handleDeleteDeck, user }) {
     <div className="container md-bg">
     <div className='big-txt txt-white'>My Decks</div>
       <div className="card-grid">
-          {deck}
-          <div className="card shdo-dk" id="card-attributes" style={{width: "17rem", justifySelf:"center"}}>
-            <Link className="card-body lt-bg-1 big-txt txt-dk" to={{pathname: '/new'}}>Create a Deck</Link>
+          <div className="card shdo-dk lt-bg-1" id="card-attributes" style={{width: "17rem", justifySelf:"center"}}>
+            <div className="flx-ctr-down">
+              <Link className="lt-bg-1 big-txt txt-dk" to={{pathname: '/new'}}>Create a Deck
+                <div className="big-txt" txt-dk>+</div>
+              </Link>
+            </div>
           </div>
+          {deck}
         </div>
     </div>
     </>
